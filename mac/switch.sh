ddf1fd752206f9afbd762088419dc6f18ba4f324
Clay Mitchell <clay@pfd.net>
2024-03-19 12:14:57 -0400

    made the scripts less dumb

--- mac/switch.sh
+++ mac/switch.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-# KYB_CH = channel on unifying receiver for the destination keyboard. Typically maps to the 1 minus the key # you press to switch keyboards
-KYB_CH=0x00
-# KYB_ID = ID of the keyboard (typically this maps to the order of keyboard devices you see in the unifying software app in Advanced settings)
-KYB_ID=0x03
+if [ -z "$1" ]
+  then
+    echo "No channel supplied"
+    exit 1
+fi
 
-# MSE_CH = channel on unifying receiver for the destination mouse. Typically maps 1 minus the channel button you press on the mouse to switch
-MSE_CH=0x00
-# MSE_ID = ID of the mouse (typically this maps to the order of mouse devices you see in the unifying software app in Advanced settings)
-MSE_ID=0x02
+# Default to channel 1
+CHANNEL=0x0$1
 
-# These typically don't change, but tap Apple menu->About This Mac->System Report->USB->USB Receiver for Logitech 
-# and make sure Product ID (RCVR_PID) and Vendor ID (RCVR_VID) are accurate 
-RCVR_VID=046D
-RCVR_PID=C52B
+echo "Changing channels to $(($1+1))"
 
-# Switch MX Keys to other device
-#                                                                                                                 A        B    C    D       E       F    G
-hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,${KYB_ID},0x09,0x1e,${KYB_CH},0x00,0x00
-# Switch MX Mouse to other device
+# Vendor and product ids for MX Mechanical and MX Master 3
+LOGI_VID=046D
+KYB_PID=B366
+MSE_PID=B034
+
+# Switch MX Mechanical to Channel 1
+#
+sudo hidapitester --vidpid ${LOGI_VID}:${KYB_PID} --usage 0x0202 --usagePage 0xff43 --open --length 20 --send-output 0x11,0x00,0x09,0x1E,${CHANNEL},0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+
+# Switch MX Master 3 to Channel 1
 #                                                                                                                 A       B       C   D       E      F    G
-hidapitester --vidpid ${RCVR_VID}:${RCVR_PID} --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,${MSE_ID},0x0a,0x1b,${MSE_CH},0x00,0x00
-
+sudo hidapitester --vidpid ${LOGI_VID}:${MSE_PID} --usage 0x0202 --usagePage 0xFF43 --open --length 20 --send-output 0x11,0xFF,0x0a,0x1b,${CHANNEL},0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
 exit 0
-
-# I have the same script on my other device but with different CH/IDs
-# KYB_CH=0x01
-# MSE_CH=0x01
-# KYB_ID=0x03
-# MSE_ID=0x01
 
 
 
